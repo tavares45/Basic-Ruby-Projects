af3e1b96309f84c1a5549fe4e55188ca23b72eed
tavares45 <161387548+tavares45@users.noreply.github.com>
2024-10-13 15:50:20 -0300

    method to calculate best buy/sell days
    Implemented the `stock_picker` method to analyze an array of stock prices 
    and determine the optimal days to buy and sell to maximize profit. 
    
    The method iterates through the prices, keeping track of the minimum price 
    and calculating potential profits to identify the best buy (minimum price day) 
    and sell (maximum price day) days.

---
+++ stock_picker.rb
@@ -0,0 +1,22 @@
+stock_prices = [17, 3, 6, 9, 15, 8, 6, 1, 10]
+
+def stock_picker(prices)
+  best_buy_day = 0
+  max_profit = 0
+  best_days = [0, 0]
+
+  prices.each_with_index do |current_price, current_day|
+    best_buy_day = current_day if current_price < prices[best_buy_day]
+
+    potential_profit = current_price - prices[best_buy_day]
+
+    if potential_profit > max_profit
+      max_profit = potential_profit
+      best_days = [best_buy_day, current_day]
+    end
+  end
+
+  best_days
+end
+
+puts "Best days are: #{stock_picker(stock_prices)}"
